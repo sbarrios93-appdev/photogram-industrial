4cb8a421b57aa6864d2ff8b3826796dce3a25daf
sbarrios93 <sbarrios93@gmail.com>
2023-01-14 20:08:01 +0000

    setup users#show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "photos#index"
+
   # devise
   devise_for :users
   # resources
@@ -7,5 +8,5 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :likes
   resources :photos
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: :show
 end
